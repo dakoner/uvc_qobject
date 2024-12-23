2c3d69ff574474ff283edfaa8c923dbbf01dd4bc
davidek <dakoner@gmail.com>
2024-12-23 14:50:55 -0800

    use root namespace scoping.

--- src/uvc_qobject.cpp
+++ src/uvc_qobject.cpp
@@ -2,27 +2,27 @@
 
 UVCQObject::UVCQObject()
 {
-    uvc_error res = uvc_init(&ctx_, NULL);
+    ::uvc_error res = ::uvc_init(&ctx_, NULL);
 
     if (res < 0)
     {
-        uvc_perror(res, "uvc_init");
+        ::uvc_perror(res, "::uvc_init");
         throw std::runtime_error("Failed to initialize");
     }
 }
 
 UVCQObject::~UVCQObject()
 {
-    uvc_exit(ctx_);
+    ::uvc_exit(ctx_);
     puts("UVC exited");
 }
 
-uvc_error UVCQObject::find_device(uvc_device_t **device, int vid, int pid, const char *sn)
+::uvc_error UVCQObject::find_device(::uvc_device_t **device, int vid, int pid, const char *sn)
 {
-    uvc_error_t res = uvc_find_device(ctx_, device, vid, pid, sn);
+    ::uvc_error_t res = ::uvc_find_device(ctx_, device, vid, pid, sn);
     if (res < 0)
     {
-        uvc_perror(res, "uvc_find_devices"); /* no devices found */
+        ::uvc_perror(res, "::uvc_find_devices"); /* no devices found */
         return res;
     }
     else
@@ -32,29 +32,29 @@ uvc_error UVCQObject::find_device(uvc_device_t **device, int vid, int pid, const
     return UVC_SUCCESS;
 }
 
-QList<uvc_device_t *> UVCQObject::find_devices()
+QList<::uvc_device_t *> UVCQObject::find_devices()
 {
-    uvc_device_t **devs;
+    ::uvc_device_t **devs;
 
-    uvc_error_t res = uvc_find_devices(ctx_, &devs, 0, 0, NULL);
+    ::uvc_error_t res = ::uvc_find_devices(ctx_, &devs, 0, 0, NULL);
     if (res < 0)
     {
-        uvc_perror(res, "uvc_find_devices"); /* no devices found */
+        ::uvc_perror(res, "::uvc_find_devices"); /* no devices found */
     }
     else
     {
         puts("Devices found");
     }
-    uvc_device_t *d = *devs;
+    ::uvc_device_t *d = *devs;
     // select device by index
     int dev_idx = 0;
-    QList<uvc_device_t *> devices;
+    QList<::uvc_device_t *> devices;
 
     while (devs[dev_idx] != NULL)
     {
-        uvc_device_descriptor *desc;
-        uvc_get_device_descriptor(devs[dev_idx], &desc);
-        printf("Device: %u %u\n", uvc_get_bus_number(devs[dev_idx]), uvc_get_device_address(devs[dev_idx]));
+        ::uvc_device_descriptor *desc;
+        ::uvc_get_device_descriptor(devs[dev_idx], &desc);
+        printf("Device: %u %u\n", ::uvc_get_bus_number(devs[dev_idx]), ::uvc_get_device_address(devs[dev_idx]));
         printf("Descriptor: %d %d %s %s %s\n", desc->idVendor, desc->idProduct, desc->serialNumber, desc->manufacturer, desc->product);
         devices.push_back(devs[dev_idx]);
         dev_idx++;
@@ -62,14 +62,14 @@ QList<uvc_device_t *> UVCQObject::find_devices()
     return devices;
 }
 
-uvc_error_t UVCQObject::open_device(uvc_device_t *device, uvc_device_handle **devh)
+::uvc_error_t UVCQObject::open_device(::uvc_device_t *device, ::uvc_device_handle **devh)
 {
     /* Try to open the device: requires exclusive access */
-    uvc_error_t res = uvc_open(device, devh);
+    ::uvc_error_t res = ::uvc_open(device, devh);
 
     if (res < 0)
     {
-        uvc_perror(res, "uvc_open"); /* unable to open device */
+        ::uvc_perror(res, "::uvc_open"); /* unable to open device */
         return res;
     }
     else
@@ -79,18 +79,18 @@ uvc_error_t UVCQObject::open_device(uvc_device_t *device, uvc_device_handle **de
     return UVC_SUCCESS;
 }
 
-QList<FormatAndFrameDescriptors *> *UVCQObject::get_formats(uvc_device_handle *devh)
+QList<FormatAndFrameDescriptors *> *UVCQObject::get_formats(::uvc_device_handle *devh)
 {
     QList<FormatAndFrameDescriptors *> *lf = new QList<FormatAndFrameDescriptors *>;
 
-    const uvc_format_desc_t *format_desc = uvc_get_format_descs(devh);
+    const ::uvc_format_desc_t *format_desc = ::uvc_get_format_descs(devh);
 
     while (format_desc != NULL)
     {
         FormatAndFrameDescriptors *f = new FormatAndFrameDescriptors;
         f->devh = devh;
         f->format_desc = format_desc;
-        const uvc_frame_desc_t *frame_desc = format_desc->frame_descs;
+        const ::uvc_frame_desc_t *frame_desc = format_desc->frame_descs;
         while (frame_desc != NULL)
         {
             f->frame_desc.push_back(frame_desc);
@@ -102,15 +102,15 @@ QList<FormatAndFrameDescriptors *> *UVCQObject::get_formats(uvc_device_handle *d
     return lf;
 }
 
-void UVCQObject::close_device(uvc_device_handle *devh)
+void UVCQObject::close_device(::uvc_device_handle *devh)
 {
-    uvc_close(devh);
+    ::uvc_close(devh);
 }
 
-void UVCQObject::cb(uvc_frame *frame, void *ptr)
+void UVCQObject::cb(::uvc_frame *frame, void *ptr)
 {
-    uvc_frame_t *bgr;
-    uvc_error_t ret;
+    ::uvc_frame_t *bgr;
+    ::uvc_error_t ret;
 
     if (frame->frame_format == UVC_COLOR_FORMAT_YUYV)
     {
@@ -119,17 +119,17 @@ void UVCQObject::cb(uvc_frame *frame, void *ptr)
             printf("Invalid frame\n");
             return;
         }
-        bgr = uvc_allocate_frame(frame->width * frame->height * 3);
+        bgr = ::uvc_allocate_frame(frame->width * frame->height * 3);
         if (!bgr)
         {
             printf("unable to allocate bgr frame!\n");
             return;
         }
-        ret = uvc_any2bgr(frame, bgr);
+        ret = ::uvc_any2bgr(frame, bgr);
         if (ret)
         {
-            uvc_perror(ret, "uvc_any2bgr");
-            uvc_free_frame(bgr);
+            ::uvc_perror(ret, "::uvc_any2bgr");
+            ::uvc_free_frame(bgr);
             return;
         }
         emit ((UVCQObject *)ptr)->frameChanged(bgr);
@@ -137,11 +137,11 @@ void UVCQObject::cb(uvc_frame *frame, void *ptr)
 }
 
 
-uvc_error UVCQObject::stream(uvc_device_handle *devh, uvc_frame_format frame_format, int width, int height, int fps)
+::uvc_error UVCQObject::stream(::uvc_device_handle *devh, ::uvc_frame_format frame_format, int width, int height, int fps)
 {
-    uvc_stream_ctrl_t ctrl;
+    ::uvc_stream_ctrl_t ctrl;
 
-    uvc_error res = uvc_get_stream_ctrl_format_size(
+    ::uvc_error res = ::uvc_get_stream_ctrl_format_size(
         devh, &ctrl, /* result stored in ctrl */
         frame_format,
         width, height, fps /* width, height, fps */
@@ -150,19 +150,19 @@ uvc_error UVCQObject::stream(uvc_device_handle *devh, uvc_frame_format frame_for
 
     if (res < 0)
     {
-        uvc_perror(res, "uvc_get_stream_ctrl_format_size"); /* device doesn't provide a matching stream */
+        ::uvc_perror(res, "::uvc_get_stream_ctrl_format_size"); /* device doesn't provide a matching stream */
         return res;
     }
     
     /* Print out the result */
-    uvc_print_stream_ctrl(&ctrl, stderr);
+    ::uvc_print_stream_ctrl(&ctrl, stderr);
     
-    res = uvc_start_streaming(devh, &ctrl, &UVCQObject::cb, this, 0);
+    res = ::uvc_start_streaming(devh, &ctrl, &UVCQObject::cb, this, 0);
 
     if (res < 0)
     {
         
-        uvc_perror(res, "start_streaming"); /* unable to start stream */
+        ::uvc_perror(res, "start_streaming"); /* unable to start stream */
         return res;
     }
     
--- src/uvc_qobject.h
+++ src/uvc_qobject.h
@@ -9,9 +9,9 @@
 class FormatAndFrameDescriptors
 {
 public:
-    uvc_device_handle *devh;
-    uvc_format_desc_t const *format_desc;
-    QList<uvc_frame_desc_t const *> frame_desc;
+    ::uvc_device_handle *devh;
+    ::uvc_format_desc_t const *format_desc;
+    QList<::uvc_frame_desc_t const *> frame_desc;
 };
 
 class UVCQObject : public QObject
@@ -21,18 +21,18 @@ class UVCQObject : public QObject
 public:
     UVCQObject();
     ~UVCQObject();
-    QList<uvc_device_t *> find_devices();
-    uvc_error find_device(uvc_device_t **device, int vid, int pid, const char *sn);
-    uvc_error open_device(uvc_device_t *device, uvc_device_handle_t **devh);
-    void close_device(uvc_device_handle_t *devh);
-    QList<FormatAndFrameDescriptors *> *get_formats(uvc_device_handle *devh);
-    uvc_error stream(uvc_device_handle *devh, uvc_frame_format frame_format, int width, int height, int fps);
-    static void cb(uvc_frame *frame, void *ptr);
+    QList<::uvc_device_t *> find_devices();
+    uvc_error find_device(::uvc_device_t **device, int vid, int pid, const char *sn);
+    uvc_error open_device(::uvc_device_t *device, ::uvc_device_handle_t **devh);
+    void close_device(::uvc_device_handle_t *devh);
+    QList<FormatAndFrameDescriptors *> *get_formats(::uvc_device_handle *devh);
+    uvc_error stream(::uvc_device_handle *devh, ::uvc_frame_format frame_format, int width, int height, int fps);
+    static void cb(::uvc_frame *frame, void *ptr);
 
 private:
-    uvc_context_t *ctx_;
+    ::uvc_context_t *ctx_;
 signals:
-    void frameChanged(uvc_frame *frame);
+    void frameChanged(::uvc_frame *frame);
 };
 
 #endif // !_UVC_THREAD_H

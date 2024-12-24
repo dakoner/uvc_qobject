bd7c30f8f2965ed462070fdea71053c45bd48a55
davidek <dakoner@gmail.com>
2024-12-23 17:31:36 -0800

    naming cleanup

--- app/main.cpp
+++ app/main.cpp
@@ -30,23 +30,3 @@ int main(int argc, char *argv[])
     w.show(); 
     return a.exec();
 }
-
-//     //     QList<uvc_device_t *> devices = uvc_qobject.find_devices();
-//     //     uvc_device_handle *devh = NULL;
-//     //     QMap<uvc_device_handle *, QList<FormatAndFrameDescriptors *> *> device_to_format_and_frame_descriptors;
-
-//     //     foreach (auto device, devices)
-//     //     {
-//     //         if (uvc_qobject.open_device(device, &devh) == UVC_SUCCESS)
-//     //         {
-//     //             // uvc_print_diag(devh, stderr);
-//     //             QList<FormatAndFrameDescriptors *> *lf = uvc_qobject.get_formats(devh);
-//     //             device_to_format_and_frame_descriptors[devh] = lf;
-//     //             uvc_qobject.close_device(devh);
-//     //         }
-//     //     }
-//     //     //uvc_qobject.stream(devh, frame_format, 1280, 720, 120 )
-
-//     QTimer::singleShot(1000, &app, &QCoreApplication::quit); // Quit after 1 second (1000 milliseconds)
-//     return app.exec();
-// }
--- uvc_qobject/uvc_qobject.pro
+++
@@ -1,9 +0,0 @@
-# QT += widgets
-
-HEADERS       = uvc_qobject.h
-SOURCES       = uvc_qobject.cpp \
-                main.cpp
-
-# # install
-# target.path = $$[QT_INSTALL_EXAMPLES]/widgets/widgets/uvc_qobject
-# INSTALLS += target

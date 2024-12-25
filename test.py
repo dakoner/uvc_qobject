fea309a33090f5b2f2828a010a6e27d449a4ac63
davidek <dakoner@gmail.com>
2024-12-25 10:03:24 -0800

    make python callback work

--- test.py
+++ test.py
@@ -1,5 +1,24 @@
+import time
 import QUVCObject
-q = QUVCObject.QUVCObject()
-print(q)
-print(dir(q))
-print(q.find_devices())
+import sys
+from PyQt6.QtCore import QCoreApplication, QTimer
+def frameChanged(*args):
+    print("frame", *args)
+
+
+
+if __name__ == "__main__":
+    app = QCoreApplication(sys.argv)
+
+    print("Hello from QCoreApplication!")
+    q = QUVCObject.QUVCObject()
+    d = QUVCObject.UVCDevice()
+    print(q.find_device(d, 0x4b4, 0x477, None))
+    dh = QUVCObject.UVCDeviceHandle()
+    q.open_device(d, dh)
+    format = 3
+    q.frameChanged.connect(frameChanged)
+    q.stream(dh, format, 1280, 720, 120)
+    t = QTimer()
+    t.singleShot(10000, app.quit)
+    sys.exit(app.exec())
